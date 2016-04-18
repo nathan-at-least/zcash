37a09167c58c527a76d63ccd734d9494ef30a312
Nathan Wilcox <nejucomo@gmail.com>
2016-04-17 17:25:47 -0700

    Add an --enable-lcov option to zcutil/build.sh.

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-set -ex
+set -exu -o pipefail
 
 cd "$(dirname "$(readlink -f "$0")")/.."
+
+# If --enable-lcov is the first argument, enable lcov coverage support:
+LCOV_ARG=''
+if [ "x${1:-}" = 'x--enable-lcov' ]
+then
+    LCOV_ARG='--enable-lcov'
+    shift
+fi
 
 # BUG: parameterize the platform/host directory:
 PREFIX="$(pwd)/depends/x86_64-unknown-linux-gnu/"
 
 make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
-./configure --prefix="${PREFIX}" --with-gui=no CXXFLAGS='-O0 -g'
+./configure --prefix="${PREFIX}" --with-gui=no "$LCOV_ARG" CXXFLAGS='-O0 -g'
 make "$@" V=1
