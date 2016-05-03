9d3d9b071833063dd8d08da9ad973773a7da60eb
Nathan Wilcox <nejucomo@gmail.com>
2016-05-03 14:00:56 -0700

    Add a mock test to trigger the first failure case in CheckTransaction.

--- src/gtest/test_checktransaction.cpp
+++ src/gtest/test_checktransaction.cpp
@@ -4,6 +4,32 @@
 #include "primitives/transaction.h"
 #include "consensus/validation.h"
 
+class MockCValidationState : public CValidationState {
+    MOCK_METHOD5(DoS, bool(int, bool, unsigned char, std::string, bool corruptionIn=false));
+    MOCK_METHOD3(Invalid, bool(bool, unsigned char, std::string));
+    MOCK_METHOD1(Error, bool(std::string));
+};
+
+
+using ::testing::Return;
+
+
+TEST(CheckTransaction, reject_empty_vin_and_empty_vpour) {
+    CTransaction tx; /* vin and vpour are empty. */
+    MockCValidationState state;
+
+    ON_CALL(state, DoS())
+        .WillByDefault(Return(false));
+
+    EXPECT_CALL(state, DoS(10, false, REJECT_INVALID, "bad-txns-vin-empty"))
+        .Times(1);
+
+    bool validtxn = CheckTransaction(tx, state);
+
+    ASSERT_FALSE(validtxn);
+}
+
+
 TEST(checktransaction_tests, check_vpub_not_both_nonzero) {
     CMutableTransaction tx;
     tx.nVersion = 2;
